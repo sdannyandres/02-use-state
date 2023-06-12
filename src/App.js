e29b64f1694524ff687c9cf27eeb25435a9a01b7
sdannyandres <sdannyandres@gmail.com>
2023-06-12 11:42:47 +0200

    use-state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { MiPrimerEstado } from './components/MiPrimerEstado';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+       <h1>El estado en React - Hook useState</h1>
+
+       <hr/>
+       <MiPrimerEstado/>
+
       </header>
     </div>
   );
---
+++ src/components/MiPrimerEstado.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+export const MiPrimerEstado = () => {
+
+  /*let nombre = "Danny Salazar";
+  
+  const cambiarNombre = e => {
+        nombre = "Juan MORATA";
+  };
+  */
+
+const [nombre, setNombre] = useState ("Danny Salazar");
+
+const cambiarNombre = (e,nombreFijo) => {
+setNombre(nombreFijo);
+console.log(e.target)
+}
+
+  return (
+    <div>
+        <h3>
+            Componente: MiPrimerEstado
+        </h3>
+    <strong className='label'>{nombre}</strong>
+        &nbsp;
+        <button onClick={ e => cambiarNombre (e,"Jesucristo")}>cambiar nombre por Jesucristo</button>
+        &nbsp;
+        <input type='text' onKeyUp={e => cambiarNombre(e, e.target.value)} placeholder='Cambia el nombre'/>
+        
+        </div>
+  )
+}
